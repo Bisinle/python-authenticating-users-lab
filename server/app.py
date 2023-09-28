12bcd34d01f98e9c39546a139edc44bffa5966b5
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-28 17:04:00 +0300

    authenticated and passed all the tests

--- server/app.py
+++ server/app.py
@@ -18,6 +18,30 @@ db.init_app(app)
 
 api = Api(app)
 
+
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username==request.get_json()['username']
+            ).first()
+        session['user_id'] = user.id
+        return make_response(jsonify(user.to_dict()),200)
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response(jsonify({'message':'204, NO C O N T E N T '}),204)
+
+class CheckSession(Resource):
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter(User.id == session.get('user_id')).first()
+            return make_response(jsonify(user.to_dict()),200)
+        else:
+            return make_response(jsonify({}),401)
+        
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -47,7 +71,13 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
 
+
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession,'/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
